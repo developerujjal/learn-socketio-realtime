3c8a5107ae3e86a372c6eb826a53db94ea36c836
developerujjal <ujjal.vision02@gmail.com>
2025-04-01 22:51:10 +0600

    first commit

---
+++ chat-room-app-backend/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const port = process.env.PORT | 5000;
+const httpServer = createServer(app);
+
+
+const allowOrigin = [
+    'http://localhost:5173'
+]
+
+const corsOptions = {
+    origin: allowOrigin, // Your React client's URL
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Best for REST APIs
+    optionsSuccessStatus: 200, // For legacy browser support
+    credentials: true
+};
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: allowOrigin,
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
+
+
+
+app.use(cors(corsOptions));
+app.use(express.json())
+
+
+io.on('connection', (socket) => {
+    console.log('new user connected:', socket.id );
+    socket.emit('welcome', 'Welcome to everyone!')
+
+    io.emit('hello', `New Guest Join ${socket.id}`);
+
+    socket.on('message', (data) => {
+        console.log(data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected!', socket.id)
+    })
+})
+
+
+
+httpServer.listen(port, () => [
+    console.log(`Server onpen in ${port} port`)
+])
---
+++ chat-room-app/src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+const App = () => {
+  // const socket = useMemo(() => {
+  //   io("http://localhost:5000")
+  // }, []);
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("welcome", (data) => {
+      console.log(data);
+    });
+
+    socket.on("hello", (data) => {
+      console.log(data);
+    });
+
+    // return () => {
+    //   socket.disconnect()
+    // }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="p-4 bg-white border-t">
+        <div className="flex space-x-2">
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type a message..."
+            className="flex-1 px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <button
+            type="submit"
+            className="bg-indigo-600 cursor-pointer text-white px-4 py-2 rounded-full hover:bg-indigo-700 transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
